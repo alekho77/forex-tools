a77a48f63453b1c0a19c696ed4c56536353bdc1c
Aleksey Khozin <Aleksey.Khozin@transas.com>
2017-10-04 21:14:21 +0300

    Draft of lsa app command line options parsing.

--- src/lsa/lsa.cpp
+++ src/lsa/lsa.cpp
@@ -1,6 +1,9 @@
 #include "fxlib/fxlib.h"
 
+#pragma warning(push)
+#pragma warning(disable:4505)  // warning C4505: unreferenced local function has been removed
 #include <boost/program_options.hpp>
+#pragma warning(pop)
 #include <boost/filesystem.hpp>
 
 #include <iostream>
@@ -13,6 +16,7 @@ using boost::program_options::store;
 using boost::program_options::notify;
 using boost::program_options::error;
 using boost::program_options::command_line_parser;
+using boost::program_options::bool_switch;
 
 void PrintCommandLineOptions(const std::vector<options_description>& opts) {
   using namespace std;
@@ -23,9 +27,34 @@ void PrintCommandLineOptions(const std::vector<options_description>& opts) {
   cout << endl << "Command line options:" << endl << desc << endl;
 }
 
-bool TryParseCommandLine(int /*argc*/, char* /*argv*/[], variables_map& /*vm*/) {
+bool TryParseCommandLine(int argc, char* argv[], variables_map& vm) {
   using namespace std;
-
+  options_description basic_desc("Basic options", 200);
+  basic_desc.add_options()
+    ("help,h", "Show help");
+  options_description generic_desc("Generic analyze options", 200);
+  generic_desc.add_options()
+    ("source,s",value<string>()->required()->value_name("pair-bin"),"Path to compiled (binary) quotes.")
+    ("quick,q", bool_switch()->default_value(true), "Start a quick (simple) analyze.");
+  options_description quick_desc("Quick analyze options", 200);
+  quick_desc.add_options()
+    ("position,p", value<string>()->required()->value_name("{long|short}"), "What position to be analyzed.")
+    ("timeout,t", value<string>()->required()->value_name("n{m,h,d,w}"), "How far to look into future (minutes, hours, days, weeks).");
+  try {
+    store(command_line_parser(argc, argv).options(basic_desc).allow_unregistered().run(), vm);
+    notify(vm);
+    if (vm.count("help")) {
+      PrintCommandLineOptions({basic_desc, generic_desc, quick_desc});
+      return false;
+    }
+    //options_description desc;
+    //store(parse_command_line(argc, argv, desc.add(generic_desc)), vm);
+    //notify(vm);
+  } catch (const error& e) {
+    cout << "[ERROR] Command line: " << e.what() << endl;
+    PrintCommandLineOptions({basic_desc, generic_desc, quick_desc});
+    return false;
+  }
   return true;
 }
 
