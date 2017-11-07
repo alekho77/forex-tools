11ea41de131c3060215307d34a6898d844671e6b
Aleksey Khozin <Aleksey.Khozin@transas.com>
2017-11-07 17:49:16 +0300

    Sketches of forex analysis that will be moved to common lib.

--- src/fxlib/fxmath.h
+++ src/fxlib/fxmath.h
@@ -1,5 +1,22 @@
 #pragma once
 
+#include <vector>
+
 namespace fxlib {
+
+struct fxrate_sample {
+  double rate;    // Expected rate
+  double period;  // Time of awaiting that rate, min
+};
+
+using fxrate_samples = std::vector<fxrate_sample>;
+
+struct fxdensity_range {
+  double rate_low;  // Lower bound of rate range
+  double rate_up;   // Upper bound of rate range
+  size_t count;     // Number of samples that are match the rate range
+};
+
+using fxrate_distribution = std::vector<fxdensity_range>;
 
 }  // namespace fxlib
