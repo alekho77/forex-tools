e9dcf1657a001ae2c320f5322f06aae6a75a880d
Aleksey Khozin <Aleksey.Khozin@transas.com>
2017-11-08 17:17:56 +0300

    First implementations of statistical methods.

--- src/fxlib/fxmath.cpp
+++ src/fxlib/fxmath.cpp
@@ -23,55 +23,32 @@ void RateStats(const fxrate_samples& samples, double& mean, double& variance) {
   variance = std::sqrt(variance / (samples.size() - 1));
 }
 
-fxrate_distribution BuildDistribution(fxrate_samples& samples, double mean, double variance, size_t distr_size) {
+fxrate_distribution BuildDistribution(fxrate_samples& samples, size_t distr_size, const double rate_from, const double rate_step) {
   using namespace std;
-  sort(samples.begin(), samples.end(), fxrate_sample_less);
-  const double ro = 0;
-  const double dr = 6 * variance / distr_size;
-  fxrate_distribution distrib;
-  distrib.reserve(distr_size + 3);  // there are two extra data
   using citer = fxrate_samples::const_iterator;
-  citer iter = samples.cbegin();
   auto count = [](citer& iter, const citer& end, fxdensity_range& density) {
-    while ((iter < end) && (iter->rate <= density.rate_up)) {
+    while ((iter < end) && (iter->rate < density.rate_up)) {
       ++density.count;
       ++iter;
     }
   };
-  const double bottom_bound = ro - dr;
-  fxdensity_range data_before = {-numeric_limits<double>::infinity(), 0.0, 0};
-  count(iter, samples.cend(), data_before);
-  if (limit_iter > limits.cbegin()) {
-    cout << "[ERROR] There are " << limits_data_before << " extra data in limits before " << fixed << setprecision(3) << (bottom_bound / g_pip) << " value" << endl;
-    throw logic_error("Something has gone wrong!");
+
+  sort(samples.begin(), samples.end(), fxrate_sample_less);
+
+  fxrate_distribution distrib;
+  distrib.reserve(distr_size + 3);  // there are two extra data and (distr_size+1) values
+  citer iter = samples.cbegin();
+  
+  distrib.push_back({-numeric_limits<double>::infinity(), rate_from, 0});
+  count(iter, samples.cend(), distrib.back());  // checking data before rate_from
+
+  for (size_t i = 0; i <= distr_size; i++) {
+    distrib.push_back({rate_from + i * rate_step, rate_from + (i + 1) * rate_step, 0});
+    count(iter, samples.cend(), distrib.back());
   }
-  int losses_data_before = 0;
-  count(loss_iter, losses.cend(), bottom_bound, losses_data_before);
-  if (loss_iter > losses.cbegin()) {
-    cout << "[ERROR] There are " << losses_data_before << " extra data in losses before " << fixed << setprecision(3) << (bottom_bound / g_pip) << " value" << endl;
-    throw logic_error("Something has gone wrong!");
-  }
-  for (size_t i = 0; i < distrib.size(); i++) {
-    get<0>(distrib[i]) = vo + i * dv;
-    count(limit_iter, limits.cend(), get<0>(distrib[i]), get<1>(distrib[i]));
-    count(loss_iter, losses.cend(), get<0>(distrib[i]), get<2>(distrib[i]));
-  }
-  if (limit_iter < limits.cend()) {
-    cout << "[NOTE] There are " << (limits.cend() - limit_iter) << " extra data in limits beyond " << fixed << setprecision(3) << (distrib.size() * dv / g_pip) << " value" << endl;
-  }
-  if ((limits_data_before
-       + accumulate(distrib.cbegin(), distrib.cend(), 0, [](auto a, const auto& b) { return a + get<1>(b); })
-       + (limits.cend() - limit_iter)) != static_cast<int>(limits.size())) {
-    throw logic_error("Sum of limits distribution is not equal the total limits!");
-  }
-  if (loss_iter < losses.cend()) {
-    cout << "[NOTE] There are " << (losses.cend() - loss_iter) << " extra data in losses beyond " << fixed << setprecision(3) << (distrib.size() * dv / g_pip) << " value" << endl;
-  }
-  if ((losses_data_before
-       + accumulate(distrib.cbegin(), distrib.cend(), 0, [](auto a, const auto& b) { return a + get<2>(b); })
-       + (losses.cend() - loss_iter)) != static_cast<int>(losses.size())) {
-    throw logic_error("Sum of losses distribution is not equal the total losses!");
-  }
+
+  distrib.push_back({rate_from + (distr_size + 1) * rate_step, numeric_limits<double>::infinity(), static_cast<size_t>(samples.cend() - iter)});  // remaining data beyond the interval
+
   return distrib;
 }
 
