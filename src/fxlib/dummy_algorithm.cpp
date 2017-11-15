2db0b155a95933ee9aa793c85b631112d891d0bd
Aleksey Khozin <Aleksey.Khozin@transas.com>
2017-11-15 20:08:43 +0300

    Implementation of dummy algorithm.

--- src/fxlib/dummy_algorithm.cpp
+++ src/fxlib/dummy_algorithm.cpp
@@ -1,12 +1,20 @@
 #include "dummy_algorithm.h"
 
-#include <random>
-
 namespace fxlib {
 
-DummyAlgorithm::DummyAlgorithm(const ForecastInfo& info) : info_(info) {
-  std::random_device rd;
-  std::mt19937 gen(rd());
+DummyAlgorithm::DummyAlgorithm(const ForecastInfo& info)
+  : info_(info)
+  , gen_(std::random_device()())
+  , dis_(1, info.window) {
+}
+
+fxforecast DummyAlgorithm::Feed(const fxcandle&) {
+  int val = dis_(gen_);
+  return val == (info_.window / 2) ? fxforecast::positive : fxforecast::negative;
+}
+
+void DummyAlgorithm::Reset() {
+  gen_ = std::mt19937(std::random_device()());
 }
 
 }  // namespace fxlib
--- src/fxlib/dummy_algorithm.h
+++ src/fxlib/dummy_algorithm.h
@@ -2,13 +2,20 @@
 
 #include "fxforecast.h"
 
+#include <random>
+
 namespace fxlib {
 
 class DummyAlgorithm : public IForecaster {
 public:
   explicit DummyAlgorithm(const ForecastInfo& info);
+  fxforecast Feed(const fxcandle&) override;
+  void Reset() override;
+  ForecastInfo Info() const override { return info_; }
 private:
   const ForecastInfo info_;
+  std::mt19937 gen_;
+  std::uniform_int_distribution<> dis_;
 };
 
 }  // namespace fxlib
