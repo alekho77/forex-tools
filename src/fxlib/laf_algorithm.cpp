ec28c3a9154ed53b558dcc89e57cf805f6da8112
Aleksey Khozin <ahozin@hotmail.com>
2017-12-11 15:58:26 +0300

    Sketches of LAF algorithm trainer.

--- src/fxlib/laf_algorithm.cpp
+++ src/fxlib/laf_algorithm.cpp
@@ -1,19 +1,35 @@
 #include "laf_algorithm.h"
 
 #include <boost/property_tree/ptree.hpp>
+#include <boost/regex.hpp>
 
 namespace fxlib {
 
-//namespace {
-//ForecastInfo from_cfg(const boost::property_tree::ptree& settings) {
-//  ForecastInfo info{};
-//  info.position = settings.get<std::string>("position") == "long" ? fxposition::fxlong : fxposition::fxshort;
-//  info.window = settings.get<int>("window");
-//  info.timeout = settings.get<int>("timeout");
-//  info.margin = settings.get<double>("margin");
-//  return info;
+namespace {
+
+struct laf_trainer_cfg {
+  fxposition position;
+  boost::posix_time::time_duration window;  //* Window size
+  boost::posix_time::time_duration timeout;  //* Timeout of wait
+  double margin;  //* Expected margin, in rate units
+  int inputs;  //* Number of inputs: 6, 12, 24 ...
+  int step;  //* Number of minutes that are used for each input.
+};
+
+//laf_trainer_cfg from_cfg(const boost::property_tree::ptree& settings) {
+//  const boost::regex rx_period("(\\d+)([mhdw])");
+//  laf_trainer_cfg cfg{};
+//  cfg.position = settings.get<std::string>("position") == "long" ? fxposition::fxlong : fxposition::fxshort;
+//  cfg.window = settings.get<int>("window");
+//  cfg.timeout = settings.get<int>("timeout");
+//  cfg.margin = settings.get<double>("margin");
+//  cfg.inputs = settings.get<int>("inputs");
+//  std::string sstep = settings.get<std::string>("step");
+//
+//  return cfg;
 //}
-//}
+
+}  // namespace
 
 
 LafTrainer::LafTrainer(const boost::property_tree::ptree& /*settings*/) {
--- src/fxlib/laf_algorithm.h
+++ src/fxlib/laf_algorithm.h
@@ -8,11 +8,14 @@
 
 namespace fxlib {
 
+class laf_trainer;
+
 class LafTrainer : public ITrainer {
 public:
   explicit LafTrainer(const boost::property_tree::ptree& settings);
   std::vector<double> PrepareTraningSet(const fxsequence& seq) const override;
 private:
+  laf_trainer* trainer_;
 };
 
 
