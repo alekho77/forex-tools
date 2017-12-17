411dc59ffef66a942e76d807ae465190e2ced6ae
Aleksey Khozin <ahozin@hotmail.com>
2017-12-17 13:58:30 +0300

    Added several LAF filters.

--- src/fxlib/laf_algorithm_def.cpp
+++ src/fxlib/laf_algorithm_def.cpp
@@ -12,6 +12,14 @@ std::shared_ptr<ilaf_impl> make_laf_impl(const std::string& type) {
     return std::shared_ptr<ilaf_impl>(new laf_alg<laf124_def>);
   } else if (type == "212") {
     return std::shared_ptr<ilaf_impl>(new laf_alg<laf212_def>);
+  } else if (type == "212b") {
+    return std::shared_ptr<ilaf_impl>(new laf_alg<laf212b_def>);
+  } else if (type == "224") {
+    return std::shared_ptr<ilaf_impl>(new laf_alg<laf224_def>);
+  } else if (type == "148") {
+    return std::shared_ptr<ilaf_impl>(new laf_alg<laf148_def>);
+  } else if (type == "248") {
+    return std::shared_ptr<ilaf_impl>(new laf_alg<laf248_def>);
   }
   return std::shared_ptr<ilaf_impl>();
 }
--- src/fxlib/laf_algorithm_def.h
+++ src/fxlib/laf_algorithm_def.h
@@ -43,6 +43,59 @@ struct laf212_def {
                                       + std::tuple_size<std::tuple_element_t<1, Trainer::sample_t>>::value;
 };
 
+struct laf212b_def {
+  using InputLayer = mathlib::input_layer<double, 12>;
+  using Neuron1 = mathlib::neuron<double, 12>;
+  using IndexPack1 = mathlib::index_sequence_pack_t<12>;
+  using Map1 = mathlib::make_type_pack<IndexPack1, 2>::type;
+  using HiddenLayer = mathlib::nnetwork<InputLayer, std::tuple<Neuron1, Neuron1>, Map1>;
+  using Neuron2 = mathlib::neuron<double, 2>;
+  using Map2 = mathlib::type_pack<mathlib::index_pack<0, 1>>;
+  using Network = mathlib::nnetwork<HiddenLayer, std::tuple<Neuron2>, Map2>;
+  using Trainer = mathlib::training_set<mathlib::bp_trainer, Network>;
+  static constexpr size_t sample_size = std::tuple_size<std::tuple_element_t<0, Trainer::sample_t>>::value
+                                      + std::tuple_size<std::tuple_element_t<1, Trainer::sample_t>>::value;
+};
+
+struct laf224_def {
+  using InputLayer = mathlib::input_layer<double, 24>;
+  using Neuron1 = mathlib::neuron<double, 24>;
+  using IndexPack1 = mathlib::index_sequence_pack_t<24>;
+  using Map1 = mathlib::make_type_pack<IndexPack1, 2>::type;
+  using HiddenLayer = mathlib::nnetwork<InputLayer, std::tuple<Neuron1, Neuron1>, Map1>;
+  using Neuron2 = mathlib::neuron<double, 2, mathlib::NOBIAS<double>>;
+  using Map2 = mathlib::type_pack<mathlib::index_pack<0, 1>>;
+  using Network = mathlib::nnetwork<HiddenLayer, std::tuple<Neuron2>, Map2>;
+  using Trainer = mathlib::training_set<mathlib::bp_trainer, Network>;
+  static constexpr size_t sample_size = std::tuple_size<std::tuple_element_t<0, Trainer::sample_t>>::value
+    + std::tuple_size<std::tuple_element_t<1, Trainer::sample_t>>::value;
+};
+
+struct laf148_def {
+  using InputLayer = mathlib::input_layer<double, 48>;
+  using Neuron = mathlib::neuron<double, 48>;
+  using IndexPack = mathlib::index_sequence_pack_t<48>;
+  using Map = mathlib::type_pack<IndexPack>;
+  using Network = mathlib::nnetwork<InputLayer, std::tuple<Neuron>, Map>;
+  using Trainer = mathlib::training_set<mathlib::bp_trainer, Network>;
+  static constexpr size_t sample_size = std::tuple_size<std::tuple_element_t<0, Trainer::sample_t>>::value
+    + std::tuple_size<std::tuple_element_t<1, Trainer::sample_t>>::value;
+};
+
+struct laf248_def {
+  using InputLayer = mathlib::input_layer<double, 48>;
+  using Neuron1 = mathlib::neuron<double, 48>;
+  using IndexPack1 = mathlib::index_sequence_pack_t<48>;
+  using Map1 = mathlib::make_type_pack<IndexPack1, 2>::type;
+  using HiddenLayer = mathlib::nnetwork<InputLayer, std::tuple<Neuron1, Neuron1>, Map1>;
+  using Neuron2 = mathlib::neuron<double, 2, mathlib::NOBIAS<double>>;
+  using Map2 = mathlib::type_pack<mathlib::index_pack<0, 1>>;
+  using Network = mathlib::nnetwork<HiddenLayer, std::tuple<Neuron2>, Map2>;
+  using Trainer = mathlib::training_set<mathlib::bp_trainer, Network>;
+  static constexpr size_t sample_size = std::tuple_size<std::tuple_element_t<0, Trainer::sample_t>>::value
+    + std::tuple_size<std::tuple_element_t<1, Trainer::sample_t>>::value;
+};
+
 }  // namespace details
 
 }  // namespace fxlib
